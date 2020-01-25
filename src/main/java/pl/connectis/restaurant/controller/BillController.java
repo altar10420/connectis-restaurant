40be527efa135ca2892a94dd58767480e62c615d
Piotr Śrótwa <altar10420@gmail.com>
2020-01-25 14:42:25 +0100

    Update in getBill()

--- src/main/java/pl/connectis/restaurant/controller/BillController.java
+++ src/main/java/pl/connectis/restaurant/controller/BillController.java
@@ -3,10 +3,8 @@ package pl.connectis.restaurant.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import pl.connectis.restaurant.controller.dto.BillDTO;
-import pl.connectis.restaurant.domain.BillHibernate;
 import pl.connectis.restaurant.service.BillService;
 
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/bill")
@@ -21,9 +19,8 @@ public class BillController {
 
     @GetMapping(path = "/{id}")
     public BillDTO getBill(@PathVariable("id") Long id) {
-        Optional<BillHibernate> billOptional = billService.getBill(id);
-        //TODO throw some exception if failed
-        return new BillDTO(billOptional.get());
+
+        return new BillDTO(billService.getBill(id).get());
     }
 
     @PostMapping(path = "/create")
